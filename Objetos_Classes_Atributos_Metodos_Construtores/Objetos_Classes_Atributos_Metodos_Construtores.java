e98d98812ee2074aedaf1159ce3f1c62eb14ba97
geovanebausentsi-cloud <geovanebausentsi@gmail.com>
2025-11-24 23:27:00 -0300

    Objetos_Classes_Atributos_Metodos_Construtores0

--- Objetos_Classes_Atributos_Metodos_Construtores/Objetos_Classes_Atributos_Metodos_Construtores.java
+++ Objetos_Classes_Atributos_Metodos_Construtores/Objetos_Classes_Atributos_Metodos_Construtores.java
@@ -4,11 +4,5 @@ public class Objetos_Classes_Atributos_Metodos_Construtores {
     
     //quando a classe esta dentro do mesmo pacote não temos a nescessidade de importação.
     //quando não criamos o construtor ele assume um construtor padrão
-    //pode haver mais de um construtor, o que os diferencia são os atributos
-    
-    
-    
-    
-    
-    
+    //pode haver mais de um construtor, o que os diferencia são os atributos   
 }
